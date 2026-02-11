3701588146cdd8479a5549d39e5bb16e0b1fe215
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-02-11 11:52:24 -0300

    Add files via upload

---
+++ Aulas20261/Aula01/script03.js
@@ -0,0 +1,12 @@
+function alterarFonte(fonte) {
+  //alert('Fonte alterada para ' + fonte)
+  document.getElementById('pagina').style.fontFamily = fonte
+}
+
+function colorirFundo(cor) {
+  document.getElementById('pagina').style.backgroundColor = cor
+}
+
+function colorirTexto(cor) {
+  document.getElementById('pagina').style.color = cor
+}
