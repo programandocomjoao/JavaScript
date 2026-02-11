392ac443428d2e9aa84526f4d09c4e1662d30842
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-02-11 11:48:19 -0300

    Add files via upload

--- Aulas20261/SuperMarioBros/scripts.js
+++ Aulas20261/SuperMarioBros/scripts.js
@@ -10,5 +10,26 @@ const pular = () => {
   setTimeout(() => { mario.classList.remove('pulo') }, 500)
 }
 
+const colisao = setInterval(() => {
+  const posCano = cano.offsetLeft
+  const posMario = +window.getComputedStyle(mario).bottom.replace('px', '')
+
+  if(posCano > 0 && posCano < 80 && posMario < 80) {
+    gameover.style.visibility = 'visible'
+
+    cano.style.animation = 'none'
+    cano.style.left = posCano + 'px'
+
+    mario.src = 'imagens/game-over.png'
+    mario.style.width = '76px'
+    mario.style.bottom = posMario + 'px'
+    mario.style.marginLeft = '25px'
+    mario.style.animation = 'none'
+
+    clearInterval(colisao)
+  }
+}, 10)
+
 document.addEventListener('keydown', pular)
 cima.addEventListener('click', pular)
+reset.addEventListener('click', () => { location.reload() })
