b8a63c2330798acfa30a72d9051fd98c59b6ea59
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-05-27 11:03:54 -0300

    Add files via upload

--- Aulas20261/PenseBem/scripts.js
+++ Aulas20261/PenseBem/scripts.js
@@ -10,3 +10,29 @@ let gabarito022 = new Array('B', 'D', 'A', 'B', 'A')
 let gabarito023 = new Array('B', 'A', 'D', 'B', 'B')
 let gabarito024 = new Array('B', 'A', 'D', 'B', 'D')
 let gabarito025 = new Array('C', 'C', 'D', 'D', 'A')
+
+iniciar = function() {
+  programa = prompt('Digite o código do programa(021 a 025):', '')
+
+  if(programa == '021')
+    gabarito = gabarito021
+  else if(programa == '022')
+    gabarito = gabarito022
+  else if(programa == '023')
+    gabarito = gabarito023
+  else if(programa == '024')
+    gabarito = gabarito024
+  else if(programa == '025')
+    gabarito = gabarito025
+  else {
+    alert('Código de programa inválido!\nDigite um código entre 021 e 025!')
+    iniciar()
+  }
+
+  pergunta = 1
+  tentativa = 1
+  pontuacao = 0
+
+  document.getElementById('tela1').value = `${programa}->${pergunta}:`
+  document.getElementById('tela2').value = `Tentativa ${tentativa} de 3`
+}
