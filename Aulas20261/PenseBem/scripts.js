fb23d8d6e70ad549d38e82cc4d19909182a35fae
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-05-27 10:43:08 -0300

    Add files via upload

---
+++ Aulas20261/PenseBem/scripts.js
@@ -0,0 +1,12 @@
+let programa = 0
+let pergunta = 1
+let tentativa = 1
+let pontuacao = 0
+
+let gabarito = new Array()
+
+let gabarito021 = new Array('B', 'A', 'A', 'D', 'C')
+let gabarito022 = new Array('B', 'D', 'A', 'B', 'A')
+let gabarito023 = new Array('B', 'A', 'D', 'B', 'B')
+let gabarito024 = new Array('B', 'A', 'D', 'B', 'D')
+let gabarito025 = new Array('C', 'C', 'D', 'D', 'A')
